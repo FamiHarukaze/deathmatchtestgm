cdc66d0b36c047d8548447d65b815dd7871ec6af
CaveeJohnson <caveejohnson@hotmail.com>
2014-08-05 18:03:56 +0100

    Revert "init changes to support menu"
    This reverts commit 703f99ad668e9be74b8944b1f1180fdc2542e4b5.

--- gamemodes/Team/gamemode/init.lua
+++ gamemodes/Team/gamemode/init.lua
@@ -86,23 +86,3 @@ function GM:PlayerSetHandsModel( ply, ent )
 	end
 
 end
-
-function team_1( ply )
- 
-    ply:SetTeam(1)
- 
-end
- 
-function team_2( ply )
- 
-    ply:SetTeam(2)
-end
-
-function team_3( ply )
- 
-    ply:SetTeam(3)
-end
- 
-concommand.Add( "team_1", team_1 )
-concommand.Add( "team_2", team_2 )
-concommand.Add( "team_3", team_3 )

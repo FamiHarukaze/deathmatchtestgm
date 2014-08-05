783efb52d3f7a94ee018d2e08384db73e752ca4c
CaveeJohnson <caveejohnson@hotmail.com>
2014-08-05 16:38:59 +0100

    tkmessage

--- gamemodes/Team/gamemode/init.lua
+++ gamemodes/Team/gamemode/init.lua
@@ -54,7 +54,8 @@ end
 function GM:PlayerShouldTakeDamage( ply, attacker ) 
 
     if ( (target:IsPlayer() && attacker:IsPlayer()) && (ply:Team() == attacker:Team()) ) then
- 
+        
+        attacker:PrintMessage(HUD_PRINTTALK, "Do not teamkill!")
         return false // Damage is now half of what you would normally take.
 
     end

40d8f721f5d03c83125e8970b87b7513c7795489
Liquidsocks <Liquidsocks1@hotmail.com>
2014-08-05 14:19:17 +0200

    Made more changes

--- gamemodes/tdm/gamemode/init.lua
+++ gamemodes/tdm/gamemode/init.lua
@@ -2,3 +2,23 @@ AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 
 include( "shared.lua" )
+
+function GM:PlayerConnect(name, ip)
+
+end
+
+function GM:PlayerInitialSpawn(ply)
+
+end
+
+function GM:PlayerAuthed(ply, SteamID, UniqueID)
+
+end
+
+function GM:PlayerSpawn(ply)
+	ply:SetModel( "models/player/odessa.mdl" )
+end
+
+function GM:PlayerSetModel( ply )
+	ply:SetModel( "models/player/odessa.mdl" )
+end

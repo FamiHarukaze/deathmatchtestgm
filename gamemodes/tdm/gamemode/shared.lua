3ee83a7ce6e77d75c69bc1c74c6ff1cb7e441501
CaveeJohnson <caveejohnson@hotmail.com>
2014-08-05 15:50:38 +0100

    added teams
    +teams

--- gamemodes/tdm/gamemode/shared.lua
+++ gamemodes/tdm/gamemode/shared.lua
@@ -1,8 +1,12 @@
 GM.Name = "Team Deathmatch"
-GM.Author = "N/A"
+GM.Author = "Liquid, Cave."
 GM.Email = "N/A"
 GM.Website = "N/A"
 
 function GM:Initialize()
 	self.BaseClass.Initialize( self )
 end
+
+team.SetUp( 1, "Team1", Color( 225, 125, 125, 255 ) )
+team.SetUp( 2, "Team2", Color( 125, 225, 125, 255 ) )
+team.SetUp( 3, "Team3", Color( 125, 125, 225, 255 ) )

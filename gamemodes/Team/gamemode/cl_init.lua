3ac5e132e0fb6b0fea1b4a29f4adbb52717bffca
CaveeJohnson <caveejohnson@hotmail.com>
2014-08-05 21:22:25 +0100

    more fixes

--- gamemodes/Team/gamemode/cl_init.lua
+++ gamemodes/Team/gamemode/cl_init.lua
@@ -15,7 +15,7 @@ frame:ShowCloseButton( false )
 frame:MakePopup()
  
 team_1 = vgui.Create( "DButton", frame )
-team_1:SetPos( frame:GetTall() / 3, frame:GetWide() / 3 ) --Place it half way on the tall and 5 units in horizontal
+team_1:SetPos( 10, 0 ) --Place it half way on the tall and 5 units in horizontal
 team_1:SetSize( 90, 300 )
 team_1:SetText( "Team 1" )
 team_1.DoClick = function() --Make the player join team 1
@@ -23,7 +23,7 @@ team_1.DoClick = function() --Make the player join team 1
 end
  
 team_2 = vgui.Create( "DButton", frame )
-team_2:SetPos( frame:GetTall() / 3, frame:GetWide() / 3 ) --Place it next to our previous one
+team_2:SetPos( 110, 0 ) --Place it next to our previous one
 team_2:SetSize( 90, 300 )
 team_2:SetText( "Team 2" )
 team_2.DoClick = function() --Make the player join team 2
@@ -31,7 +31,7 @@ team_2.DoClick = function() --Make the player join team 2
 end
 
 team_3 = vgui.Create( "DButton", frame )
-team_3:SetPos( frame:GetTall() / 3, frame:GetWide() / 3 ) --Place it next to our previous one
+team_3:SetPos( 210, 0 ) --Place it next to our previous one
 team_3:SetSize( 90, 300 )
 team_3:SetText( "Team 3" )
 team_3.DoClick = function() --Make the player join team 3

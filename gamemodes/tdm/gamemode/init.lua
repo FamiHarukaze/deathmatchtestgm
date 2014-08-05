cd9376fdc466e4cc92d23974e8fa93ce03b2624f
Liquidsocks <Liquidsocks1@hotmail.com>
2014-08-05 16:17:05 +0200

    Made minor changes to PlayerSpawn

--- gamemodes/tdm/gamemode/init.lua
+++ gamemodes/tdm/gamemode/init.lua
@@ -5,6 +5,25 @@ DeriveGamemode("sandbox")
 include( "shared.lua" )
 
 //Serverside stuff goes here
+local PlayerModels = {
+Model("models/Humans/Group01/Female_01.mdl"),
+Model("models/Humans/Group01/Female_02.mdl"),
+Model("models/Humans/Group01/Female_03.mdl"),
+Model("models/Humans/Group01/Female_04.mdl"),
+Model("models/Humans/Group01/Female_05.mdl"),
+Model("models/Humans/Group01/Female_06.mdl"),
+Model("models/Humans/Group01/Female_07.mdl"),
+Model("models/Humans/Group01/Male_01.mdl"),
+Model("models/Humans/Group01/Male_02.mdl"),
+Model("models/Humans/Group01/Male_03.mdl"),
+Model("models/Humans/Group01/Male_04.mdl"),
+Model("models/Humans/Group01/Male_05.mdl"),
+Model("models/Humans/Group01/Male_06.mdl"),
+Model("models/Humans/Group01/Male_07.mdl"),
+Model("models/Humans/Group01/Male_08.mdl"),
+Model("models/Humans/Group01/Male_09.mdl")
+}
+
 
 function GM:PlayerConnect(name, ip)
 
@@ -19,15 +38,14 @@ function GM:PlayerAuthed(ply, SteamID, UniqueID)
 end
 
 function GM:PlayerSpawn(ply)
-	ply:SetModel( "models/player/odessa.mdl" )
+	ply:SetModel(table.Random(PlayerModels))
 	ply:Give("weapon_crowbar")
-	ply:Give("weapon_physgun")
-	ply:Give("gmod_tool")
-end
-
-function GM:PlayerLoadout(ply)
 	if (ply:IsAdmin() || ply:IsSuperAdmin() ) then
 		ply:Give("weapon_physgun")
 		ply:Give("gmod_tool")
 	end
+end
+
+function GM:PlayerLoadout(ply)
+
 end

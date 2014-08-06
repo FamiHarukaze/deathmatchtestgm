d93b53d0301a3b21a55088d28bd25317278a4fa3
Liquidsocks <Liquidsocks1@hotmail.com>
2014-08-06 13:02:33 +0200

    Fuckink sound :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1: :-1:

--- gamemodes/Team/gamemode/sh_playsound.lua
+++ gamemodes/Team/gamemode/sh_playsound.lua
@@ -1,38 +1,43 @@
 --[[
-	=== HSP Base Plugin ===
-	sh_B_SPS, v1.3
-	Serverside surface.PlaySound!
-	Credits: HeX
+        === HSP Base Plugin ===
+        sh_B_SPS, v1.3
+        Serverside surface.PlaySound!
 ]]
 
 
 if (SERVER) then
-	local function SendUM(ply,snd)
-		umsg.Start("SPlay", ply)
-			umsg.String(snd)
-		umsg.End()
-	end
-	
-	local pMeta = FindMetaTable("Player")
-	function pMeta:SPlay(snd)
-		SendUM(self,snd)
-	end
-	
-	function SPlay(ply,snd)
-		if type(ply) == "string" then
-			SendUM(nil,ply)
-		else
-			SendUM(ply,snd)
-		end
-	end
+        local function SendUM(ply,snd)
+                umsg.Start("SPS", ply)
+                        umsg.String(snd)
+                umsg.End()
+        end
+       
+        local pMeta = FindMetaTable("Player")
+        function pMeta:SPS(snd)
+                SendUM(self,snd)
+        end
+       
+        function SPS(ply,snd)
+                if type(ply) == "string" then
+                        SendUM(nil,ply)
+                else
+                        SendUM(ply,snd)
+                end
+        end
 end
 
 
 if (CLIENT) then
-	local function SPlay(um)
-		surface.PlaySound( um:ReadString() )
-	end
-	usermessage.Hook("SPlay", SPlay)
+        local function PlaySound(um)
+                surface.PlaySound( um:ReadString() )
+        end
+        usermessage.Hook("SPS", PlaySound)
 end
 
-print("Playsound loaded")
+
+
+
+
+
+
+

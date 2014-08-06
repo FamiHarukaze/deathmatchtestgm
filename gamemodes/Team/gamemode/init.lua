dc6e77020f1d42caa4e08fe197b971d195cbf2cd
Liquidsocks <Liquidsocks1@hotmail.com>
2014-08-06 13:07:18 +0200

    yuy :dancer:

--- gamemodes/Team/gamemode/init.lua
+++ gamemodes/Team/gamemode/init.lua
@@ -114,6 +114,10 @@ local maxdeathtime = 10;
 function player_initdeath( ply, wep, killer )
  
      ply.nextspawn = CurTime() + maxdeathtime;
+	 if !(killer:IsPlayer()) then 
+	 ply:PrintMessage(HUD_PRINTTALK, "You were killed by the world")
+	 end
+	 
      if (killer:SteamID() != ply:SteamID()) then
         ply:PrintMessage(HUD_PRINTTALK,"You have been killed by " .. killer:Nick() .. ", He had " .. killer:Health() .. "HP left.")
         ply:PrintMessage(HUD_PRINTTALK,"You will auto-respawn in 10 seconds!")

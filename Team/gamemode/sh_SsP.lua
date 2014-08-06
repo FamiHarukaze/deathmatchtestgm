ca2970c058577d92d8f446786c99924d604b3af8
Liquidsocks <Liquidsocks1@hotmail.com>
2014-08-06 22:34:56 +0200

    Testing netmessages

--- Team/gamemode/sh_SsP.lua
+++ Team/gamemode/sh_SsP.lua
@@ -10,6 +10,7 @@ print("Ss.P loaded!")
 if (SERVER) then
 	util.AddNetworkString( "SPlayAll" )
 	util.AddNetworkString( "SPlay" )
+	util.AddNetworkString( "Example1" )
 	
 	function SPlay(ply, msg)
 		net.Start( "SPlay" )
@@ -22,14 +23,29 @@ if (SERVER) then
 			net.WriteString( msg )
 		net.Broadcast()
 	end
+	function NetExample()
+	net.Start( "Example1" )
+        net.WriteTable( "Hello" )
+    net.Broadcast() 
+	end
+	
+	print("Ss.P loaded serverside!")
 end
 
 if (CLIENT) then
-	net.Receive( "SPlay", function( msg )
-		surface.PlaySound( net.ReadString )
+	net.Receive( "SPlay", function( len )
+		surface.PlaySound( net.ReadString() )
 	end)
    
-    net.Receive( "SPlayAll", function( msg )
-        surface.PlaySound( net.ReadString )
+    net.Receive( "SPlayAll", function( len )
+		local sound = net.ReadString()
+        surface.PlaySound( Sound(sound) )
 	end)
+	
+	net.Receive( "Example1", function( len )
+       print( "EXAMPLE 1: Received message ( " .. len .. " bits )" )
+       print( net.ReadString() )   
+   end)
+	
+	print("Ss.P loaded clientside!")
 end

461a727cb1e3b6866ab4d104860ad33085872926
CaveeJohnson <caveejohnson@hotmail.com>
2014-08-06 10:02:24 +0100

    small fix

--- gamemodes/Team/gamemode/shared.lua
+++ gamemodes/Team/gamemode/shared.lua
@@ -10,4 +10,4 @@ end
 team.SetUp( 1, "UnDec", Color( 225, 255, 255, 255 ) )
 team.SetUp( 2, "Team1", Color( 255, 125, 125, 255 ) )
 team.SetUp( 3, "Team2", Color( 125, 255, 125, 255 ) )
-team.SetUp( 4, "Team3", Color( 125, 125, 255, 255,) )
+team.SetUp( 4, "Team3", Color( 125, 125, 255, 255 ) )

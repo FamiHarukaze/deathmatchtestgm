36a52de5ef599512d5424c500bdbc5a9ac821a6d
CaveeJohnson <caveejohnson@hotmail.com>
2014-08-06 09:57:41 +0100

    even more fixes, gui works properly now

--- gamemodes/Team/gamemode/cl_init.lua
+++ gamemodes/Team/gamemode/cl_init.lua
@@ -7,32 +7,32 @@ function set_team()
  
 local frame = vgui.Create( "DFrame" )
 frame:SetPos( ScrW() / 2, ScrH() / 2 ) --Set the window in the middle of the players screen/game window
-frame:SetSize( 300, 320 ) --Set the size
+frame:SetSize( 310, 320 ) --Set the size
 frame:SetTitle( "Change Team" ) --Set title
 frame:SetVisible( true )
 frame:SetDraggable( false )
-frame:ShowCloseButton( false )
+frame:ShowCloseButton( true )
 frame:MakePopup()
  
 team_1 = vgui.Create( "DButton", frame )
-team_1:SetPos( 10, 0 ) --Place it half way on the tall and 5 units in horizontal
-team_1:SetSize( 90, 300 )
+team_1:SetPos( 10, 31 ) --Place it half way on the tall and 5 units in horizontal
+team_1:SetSize( 90, 278 )
 team_1:SetText( "Team 1" )
 team_1.DoClick = function() --Make the player join team 1
     RunConsoleCommand( "team_1" )
 end
  
 team_2 = vgui.Create( "DButton", frame )
-team_2:SetPos( 110, 0 ) --Place it next to our previous one
-team_2:SetSize( 90, 300 )
+team_2:SetPos( 110, 31 ) --Place it next to our previous one
+team_2:SetSize( 90, 278 )
 team_2:SetText( "Team 2" )
 team_2.DoClick = function() --Make the player join team 2
     RunConsoleCommand( "team_2" )
 end
 
 team_3 = vgui.Create( "DButton", frame )
-team_3:SetPos( 210, 0 ) --Place it next to our previous one
-team_3:SetSize( 90, 300 )
+team_3:SetPos( 210, 31 ) --Place it next to our previous one
+team_3:SetSize( 90, 278 )
 team_3:SetText( "Team 3" )
 team_3.DoClick = function() --Make the player join team 3
     RunConsoleCommand( "team_3" )

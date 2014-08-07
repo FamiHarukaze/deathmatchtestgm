c0caf5528e30b6abd7e93f41ed4d425406db872f
CaveeJohnson <caveejohnson@hotmail.com>
2014-08-07 13:03:39 +0100

    Revert "RPG Mode c:"
    This reverts commit dc6dcb91fe02da07ea109b1c3ee8341a132421d1.

--- Team/gamemode/teams.lua
+++ Team/gamemode/teams.lua
@@ -3,9 +3,9 @@ local ply = FindMetaTable("Player")
 local teams = {}
 
 teams[1] = {name = "Un-Dec", color = Vector( 1.0, 1.0, 1.0 ), weapons = {} } //Add weapons here, teams can be viewed in shared.lua
-teams[2] = {name = "Team 1", color = Vector( 1.0, .2, .2 ), weapons = {"weapon_crowbar", "weapon_rpg"} }
-teams[3] = {name = "Team 2", color = Vector( .2, 1.0, .2 ), weapons = {"weapon_crowbar", "weapon_rpg"} }
-teams[4] = {name = "Team 3", color = Vector( .2, .2, 1.0 ), weapons = {"weapon_crowbar", "weapon_rpg"} }
+teams[2] = {name = "Team 1", color = Vector( 1.0, .2, .2 ), weapons = {"weapon_crowbar", "weapon_crossbow"} }
+teams[3] = {name = "Team 2", color = Vector( .2, 1.0, .2 ), weapons = {"weapon_crowbar", "weapon_crossbow"} }
+teams[4] = {name = "Team 3", color = Vector( .2, .2, 1.0 ), weapons = {"weapon_crowbar", "weapon_crossbow"} }
 
 function ply:SetGamemodeTeam( n )
 	if not teams[n] then return end
@@ -26,8 +26,7 @@ function ply:GiveGamemodeWeapons()
 	for k, wep in pairs(teams[n].weapons) do
 		self:Give(wep)
 	end
-	//self:SetAmmo( 500, "XBowBolt", true )
-	self:SetAmmo( 500, "RPG_Round", true )
+	self:SetAmmo( 500, "XBowBolt", true )
 	if (self:IsAdmin() || self:IsSuperAdmin() ) then
 		self:Give("weapon_physgun")
 		self:Give("gmod_tool")

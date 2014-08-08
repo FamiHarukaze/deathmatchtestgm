1d3a525182bf17de6d03d206c4d9f154d71987a5
Liquidsocks <Liquidsocks1@hotmail.com>
2014-08-08 11:07:31 +0200

    Added chat.lua

---
+++ Team/gamemode/chat.lua
@@ -0,0 +1,18 @@
+/*
+If /ooc then say im gay
+8 plays hac/eight.wav
+chat tags
+*/
+
+function PlayerSay(ply, txt, teamChat)
+	if string.sub( txt, 1, 4 ) == "/ooc" then // ShitRP
+		return "i am gay"
+		SPlay(ply, "qac/horns_new.mp3")
+	end
+	
+	if txt == "8" then // Eight
+		SPlayAll("qac/eight.wav")
+	end
+	//I'll add chattags later. - Liquid
+end
+hook.Add("PlayerSay", "PlayerSay", PlayerSay)
